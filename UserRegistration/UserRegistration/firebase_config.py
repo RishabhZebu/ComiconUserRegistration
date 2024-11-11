3c60bdbea735f3254283fed40496c995db0e0b1b
RishabhZebu <rishabh.g@zebuanimation.com>
2024-11-11 12:24:21 +0530

    Fixed Json Issue

--- UserRegistration/UserRegistration/firebase_config.py
+++ UserRegistration/UserRegistration/firebase_config.py
@@ -2,17 +2,12 @@ import firebase_admin,os
 from firebase_admin import credentials
 from firebase_admin import db , firestore
 import json
-import base64
 
-# Initialize Firebase Admin SDK
-# Get the base64 encoded Firebase config from environment variable
-firebase_config_base64 = os.getenv('FIREBASE_CONFIG')
+# Get the firebase config string from environment variables
+firebase_json_str = os.getenv("FIREBASE_CONFIG")
 
-# Decode the base64 string into the original JSON configuration
-firebase_config_json = base64.b64decode(firebase_config_base64).decode('utf-8')
-
-# Parse the JSON
-firebase_config = json.loads(firebase_config_json)
+# Convert the string to a JSON object
+firebase_config = json.loads(firebase_json_str)
 
 cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred, {
--- UserRegistration/UserRegistration/settings.py
+++ UserRegistration/UserRegistration/settings.py
@@ -42,7 +42,7 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
-FIREBASE_CONFIG = json.loads(os.getenv('FIREBASE_CONFIG', '{}'))
+
 
 # Other non-sensitive settings remain in settings.py
 INSTALLED_APPS = [
