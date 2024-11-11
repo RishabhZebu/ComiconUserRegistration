1fdc8d62da04eee5630a7f5af6de106ae95e4fb4
RishabhZebu <rishabh.g@zebuanimation.com>
2024-11-11 12:09:33 +0530

    Decode ENV variable

--- UserRegistration/UserRegistration/firebase_config.py
+++ UserRegistration/UserRegistration/firebase_config.py
@@ -2,9 +2,18 @@ import firebase_admin,os
 from firebase_admin import credentials
 from firebase_admin import db , firestore
 import json
+import base64
 
 # Initialize Firebase Admin SDK
-firebase_config = json.loads(os.getenv('FIREBASE_CONFIG', '{}'))
+# Get the base64 encoded Firebase config from environment variable
+firebase_config_base64 = os.getenv('FIREBASE_CONFIG')
+
+# Decode the base64 string into the original JSON configuration
+firebase_config_json = base64.b64decode(firebase_config_base64).decode('utf-8')
+
+# Parse the JSON
+firebase_config = json.loads(firebase_config_json)
+
 cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://comiconuserentry-a72ba-default-rtdb.asia-southeast1.firebasedatabase.app/'  # Replace with your Firebase Realtime Database URL
